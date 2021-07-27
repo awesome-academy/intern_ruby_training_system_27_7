636007847415287432a9d7d85b268efe31bc561e
Minh Nguyen Ngoc <mrminht2000@gmail.com>
2021-07-27 17:38:46 +0700

    Fix convention errors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from '@rails/ujs'
+import Turbolinks from 'turbolinks'
+import * as ActiveStorage from '@rails/activestorage'
+import 'channels'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import 'bootstrap';
+
+document.addEventListener('turbolinks:load', () => {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+import '../stylesheets/application'
+require('admin-lte')
+import '@fortawesome/fontawesome-free/js/all'
