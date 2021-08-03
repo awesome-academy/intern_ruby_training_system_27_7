a81c692771cc8f57d8e6ba9c9f6f2cc33ebce9f9
Le Anh Thanh <le.anh.thanh@sun-asterisk.com>
2021-08-03 14:03:45 +0700

    Trainee reports

---
+++ app/controllers/user_reports_controller.rb
@@ -0,0 +1,75 @@
+class UserReportsController < ApplicationController
+  before_action :logged_in_user
+  before_action :load_report, only: %i(show edit destroy)
+  before_action :correct_user, except: %i(index new create)
+  before_action :load_course, except: %i(index show destroy)
+
+  def index
+    @user_reports = current_user.user_reports.recent.page(params[:page])
+                                .per Settings.course_index_page
+  end
+
+  def new
+    @user_report = UserReport.new
+  end
+
+  def create
+    @user_report = current_user.user_reports.create report_params
+    return render :new unless @user_report.save
+
+    flash[:success] = t "success"
+    redirect_to user_reports_path
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user_report.update report_params
+      flash[:success] = t ".updated_success"
+      redirect_to user_reports_path
+    else
+      flash.now[:danger] = t ".updated_fail"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user_report.destroy
+      flash[:success] = t "user_report_deleted"
+    else
+      flash[:danger] = t "delete_fail"
+    end
+    redirect_back fallback_location: user_reports_path
+  end
+
+  private
+  def report_params
+    params.require(:user_report).permit UserReport::USER_REPORT_PARAMS
+  end
+
+  def load_course
+    @courses = current_user.courses
+    return unless @courses.empty?
+
+    flash[:danger] = t ".no_courses"
+    redirect_to root_url
+  end
+
+  def load_report
+    @user_report = UserReport.find_by id: params[:id]
+    return if @user_report
+
+    flash[:danger] = t "report_not_found"
+    redirect_to user_reports_path
+  end
+
+  def correct_user
+    @user_report = current_user.user_reports.find_by id: params[:id]
+    return if @user_report
+
+    flash[:danger] = t "report_invalid"
+    redirect_to user_reports_path
+  end
+end
--- app/models/user_report.rb
+++ app/models/user_report.rb
@@ -1,8 +1,15 @@
 class UserReport < ApplicationRecord
+  USER_REPORT_PARAMS = [:user_id, :course_id, :content, :date].freeze
+
   belongs_to :user
   belongs_to :course
+
+  delegate :name, to: :course, prefix: true
+  delegate :full_name, to: :user, prefix: true
 
   validates :content, presence: true,
     length: {maximum: Settings.maximum_content_length}
   validates :date, presence: true
+
+  scope :recent, ->{order(date: :desc)}
 end
